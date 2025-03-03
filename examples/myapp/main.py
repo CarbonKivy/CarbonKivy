fe833d5a15d0b3600c3cde18406d5fd88570d14d
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-03-03 08:38:05 +0530

    Update Example

--- examples/myapp/main.py
+++ examples/myapp/main.py
@@ -8,20 +8,13 @@ def set_softinput(*args) -> None:
 
 
 Window.on_restore(Clock.schedule_once(set_softinput, 0.1))
-Window.clearcolor = [1, 1, 1, 1]
 
 appkv = """
 Screen:
     CButtonPrimary:
         text: "Primary"
-        role: "Large Productive"
-        pos_hint: {'center_y': 0.9, 'center_x': 0.2}
-
-    Button:
-        background_color: app.interactive
-        text: "Button"
-        size_hint: .1, .1
-        pos_hint: {"center_x" : .5, "center_y": .5}
+        role: "Medium"
+        pos_hint: {'center_y': 0.5, 'center_x': 0.5}
 
 """
 
