e13e1105ff94db03910e04360fef1936ebb2f8fc
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-03-11 18:01:21 +0530

    Implement CIcon

--- carbonkivy/uix/icon/__init__.py
+++ carbonkivy/uix/icon/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from kivy.lang import Builder
 
-from carbonkivy.uix.icon.icon import CIcon
+from .icon import CIcon
 from carbonkivy.config import UIX
 
 Builder.load_file(os.path.join(UIX, "icon", "icon.kv"))
--- carbonkivy/uix/icon/icon.py
+++ carbonkivy/uix/icon/icon.py
@@ -1,9 +1,33 @@
+from __future__ import annotations
+
+__all__ = ("CIcon",)
+
+import os
+
+from kivy.properties import OptionProperty
 from kivy.uix.label import Label
 
-from carbonkivy.theme.icons import IBMIcon
+from carbonkivy.behaviors import AdaptivePositionBehavior
+from carbonkivy.config import DATA
+from carbonkivy.theme.icons import ibm_icons
+from carbonkivy.theme.size_tokens import font_size_tokens
+from carbonkivy.utils import APP, get_font_size
 
 
-class CIcon(IBMIcon, Label):
+class CIcon(Label, AdaptivePositionBehavior):
     """
-    The CIcon class inherits from IBMIcon and Label to display icons from IBM's icon library.
+    The CIcon class inherits from Label to display icons from IBM's icon library using the generated icon font.
     """
+
+    icon = OptionProperty("", options=ibm_icons.keys())
+
+    font_name = os.path.join(DATA, "Icons", "carbondesignicons.ttf")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_style(self, *args) -> None:
+        self.update_specs()
+
+    def on_icon(self, *args) -> None:
+        self.text = ibm_icons[self.icon]
