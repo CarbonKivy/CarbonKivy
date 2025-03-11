d6d672344be61a8870772def02ec823d736705b5
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-03-11 18:01:21 +0530

    Implement CIcon

--- carbonkivy/factory_registers.py
+++ carbonkivy/factory_registers.py
@@ -12,6 +12,7 @@ Once registered, the components can be used without explicitly importing them el
 
 # Register the component with Kivy's Factory
 register("CLabel", module="carbonkivy.uix.label")
+register("CIcon", module="carbonkivy.uix.icon")
 register("CButton", module="carbonkivy.uix.button")
 register("CButtonPrimary", module="carbonkivy.uix.button")
 register("CButtonSecondary", module="carbonkivy.uix.button")
