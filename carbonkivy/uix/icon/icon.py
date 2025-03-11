7a7200a8ac17395773fb938ff3c25aeea0013237
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-03-11 18:18:43 +0530

    Bug fixes

--- carbonkivy/uix/icon/icon.py
+++ carbonkivy/uix/icon/icon.py
@@ -7,14 +7,11 @@ import os
 from kivy.properties import OptionProperty
 from kivy.uix.label import Label
 
-from carbonkivy.behaviors import AdaptivePositionBehavior
 from carbonkivy.config import DATA
 from carbonkivy.theme.icons import ibm_icons
-from carbonkivy.theme.size_tokens import font_size_tokens
-from carbonkivy.utils import APP, get_font_size
 
 
-class CIcon(Label, AdaptivePositionBehavior):
+class CIcon(Label):
     """
     The CIcon class inherits from Label to display icons from IBM's icon library using the generated icon font.
     """
