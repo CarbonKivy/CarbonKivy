9c63f19d0d85136a2f4d5fb5381099e520d24a8c
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-03-27 13:22:49 +0530

    Update factory registers

--- carbonkivy/factory_registers.py
+++ carbonkivy/factory_registers.py
@@ -1,4 +1,9 @@
+import os
+
 from kivy.factory import Factory
+from kivy.core.text import LabelBase
+
+from carbonkivy.config import DATA
 
 # Alias for the register function from Factory
 register = Factory.register
@@ -6,7 +11,7 @@ register = Factory.register
 """
 Registers custom components to the Kivy Factory.
 
-This code registers each component within the "ui" directory to the Kivy Factory. 
+This code registers each component within the "uix" directory to the Kivy Factory. 
 Once registered, the components can be used without explicitly importing them elsewhere in the kvlang files.
 """
 
@@ -20,7 +25,20 @@ register("CButtonSecondary", module="carbonkivy.uix.button")
 register("CFloatLayout", module="carbonkivy.uix.floatlayout")
 register("CIcon", module="carbonkivy.uix.icon")
 register("CLabel", module="carbonkivy.uix.label")
+register("CLink", module="carbonkivy.uix.link")
 register("CRelativeLayout", module="carbonkivy.uix.relativelayout")
 register("CScreen", module="carbonkivy.uix.screen")
 register("CScreenManager", module="carbonkivy.uix.screenmanager")
 register("CStackLayout", module="carbonkivy.uix.stacklayout")
+
+# Alias for the register function from Factory
+font_register = LabelBase.register
+
+"""
+Registers custom fonts to the Kivy LabelBase.
+
+Once registered, the fonts can be used without explicitly importing them elsewhere in the kvlang files.
+"""
+
+# Register the font with the LabelBase
+font_register("cicon", os.path.join(DATA, "Icons", "carbondesignicons.ttf"))
