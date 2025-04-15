2dbab7d8adae09a473560764d105f3536c9c1d08
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-04-15 14:15:45 +0530

    formatting

--- carbonkivy/uix/button/__init__.py
+++ carbonkivy/uix/button/__init__.py
@@ -2,7 +2,13 @@ import os
 
 from kivy.lang import Builder
 
-from .button import CButton, CButtonPrimary, CButtonSecondary, CButtonGhost
+from .button import (
+    CButton,
+    CButtonGhost,
+    CButtonPrimary,
+    CButtonSecondary,
+    CButtonTertiary,
+)
 from carbonkivy.config import UIX
 
 Builder.load_file(os.path.join(UIX, "button", "button.kv"))
