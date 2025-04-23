4cfb230718679b78f7637e8d0fcc08db9d2f9e01
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-04-23 18:05:21 +0530

    fix bugs.

--- carbonkivy/uix/image/__init__.py
+++ carbonkivy/uix/image/__init__.py
@@ -1,1 +1,8 @@
+import os
+
+from kivy.lang import Builder
+
 from .image import CImage
+from carbonkivy.config import UIX
+
+Builder.load_file(os.path.join(UIX, "image", "image.kv"))
--- carbonkivy/uix/image/image.py
+++ carbonkivy/uix/image/image.py
@@ -4,7 +4,7 @@ __all__ = ("CImage",)
 
 from kivy.clock import Clock, mainthread
 from kivy.core.window import Window
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, NumericProperty
 from kivy.uix.image import AsyncImage
 
 from carbonkivy.behaviors import (
@@ -16,12 +16,24 @@ class CImage(AsyncImage, DeclarativeBehavior):
 
     ratio = ListProperty([4, 3])
 
+    height_ratio = NumericProperty()
+
+    width_ratio = NumericProperty()
+
     def __init__(self, *args, **kwargs):
         super(CImage, self).__init__(*args, **kwargs)
-        self.size_hint = (1, None)
+        self.on_ratio()
         self.bind(texture_size=self.adjust_image_size)
-        Window.bind(size=self.adjust_image_size)
 
-    def adjust_image_size(self, *args) -> None:
+    @mainthread
+    def on_ratio(self, *args) -> None:
         w_ratio, h_ratio = self.ratio
-        self.height = h_ratio / w_ratio * self.width
+        self.height_ratio = h_ratio / w_ratio
+        self.width_ratio = 1 / self.height_ratio
+
+    @mainthread
+    def adjust_image_size(self, *args) -> None:
+        if self.size_hint_y == None:
+            self.height = self.width * self.height_ratio
+        elif self.size_hint_x == None:
+            self.width *= self.height * self.width_ratio
