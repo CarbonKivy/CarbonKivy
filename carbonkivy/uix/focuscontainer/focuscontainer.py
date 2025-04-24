25cd1ec650b2aeabb9f64e9093577c707286d4d0
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-04-24 17:38:46 +0530

    Implement FocusContainer

---
+++ carbonkivy/uix/focuscontainer/focuscontainer.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+__all__ = ("FocusContainer",)
+
+from kivy.properties import BooleanProperty
+from kivy.uix.boxlayout import BoxLayout
+
+from carbonkivy.behaviors import (
+    AdaptiveBehavior,
+    BackgroundColorBehavior,
+    DeclarativeBehavior,
+    HoverBehavior,
+)
+
+
+class FocusContainer(
+    AdaptiveBehavior,
+    BackgroundColorBehavior,
+    BoxLayout,
+    DeclarativeBehavior,
+    HoverBehavior,
+):
+    focus = BooleanProperty(False)
+
+    def __init__(self, **kwargs):
+        super(FocusContainer, self).__init__(**kwargs)
+
+    def on_touch_down(self, touch) -> bool:
+        super().on_touch_down(touch)
+        if self.cstate != "disabled":
+            self.focus = self.collide_point(*touch.pos)
+        return super().on_touch_down(touch)
+
+    def on_focus(self, *args) -> None:
+        if self.focus:
+            self._bg_color = self.active_color
+        elif not self.hover:
+            self._bg_color = self.bg_color
+
