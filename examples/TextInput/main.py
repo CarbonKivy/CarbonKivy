a9e4d10bc5a56a174d0ce197c7314af1153a37d6
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-04-27 18:14:34 +0530

    Update Example

---
+++ examples/TextInput/main.py
@@ -0,0 +1,45 @@
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+
+def set_softinput(*args) -> None:
+    Window.keyboard_anim_args = {"d": 0.2, "t": "in_out_expo"}
+    Window.softinput_mode = "below_target"
+
+
+Window.on_restore(Clock.schedule_once(set_softinput, 0.1))
+
+appkv = """
+CScreen:
+
+    CBoxLayout:
+        size_hint: 1, 1
+        padding: [dp(16), dp(16), dp(16), dp(16)]
+
+        CTextInput:
+            pos_hint: {"center_x": 0.5, "center_y": 0.5}
+
+            CTextInputLabel:
+                text: "Label"
+
+            CTextInputHelperText:
+                text: "Helper Text"
+    
+"""
+
+from kivy.lang import Builder
+from carbonkivy.app import CarbonApp
+from carbonkivy.uix.screen import CScreen
+
+
+class myapp(CarbonApp):
+    def __init__(self, *args, **kwargs):
+        super(myapp, self).__init__(*args, **kwargs)
+
+    def build(self) -> CScreen:
+        screen = Builder.load_string(appkv)
+        return screen
+
+
+if __name__ == "__main__":
+    myapp().run()
