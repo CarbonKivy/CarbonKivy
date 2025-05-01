2fcef6124a26d87822fcf0cf371064341dfebda4
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-05-01 17:43:47 +0530

    format

--- carbonkivy/uix/textinput/__init__.py
+++ carbonkivy/uix/textinput/__init__.py
@@ -2,7 +2,12 @@ import os
 
 from kivy.lang import Builder
 
-from .textinput import CTextInput, CTextInputLabel, CTextInputHelperText, CTextInputTrailingIconButton
+from .textinput import (
+    CTextInput,
+    CTextInputLabel,
+    CTextInputHelperText,
+    CTextInputTrailingIconButton,
+)
 from carbonkivy.config import UIX
 
 Builder.load_file(os.path.join(UIX, "textinput", "textinput.kv"))
