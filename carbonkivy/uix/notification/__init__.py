b021c5f9f2b74970887fbe7fb6e5b6209987c624
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-06-13 16:57:49 +0530

    Bug fixes and Improvements

--- carbonkivy/uix/notification/__init__.py
+++ carbonkivy/uix/notification/__init__.py
@@ -1,10 +1,15 @@
 import os
 
+from carbonkivy.config import UIX
 from kivy.lang import Builder
 
-from carbonkivy.config import UIX
-
-from .notification import CBaseNotification, CNotification, CNotificationInline, CNotificationToast, CNotificationCloseButton
+from .notification import (
+    CBaseNotification,
+    CNotification,
+    CNotificationCloseButton,
+    CNotificationInline,
+    CNotificationToast,
+)
 
 filename = os.path.join(UIX, "notification", "notification.kv")
 if not filename in Builder.files:
