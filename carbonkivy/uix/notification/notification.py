e652da831b1e106caf84589ce629f2494837ac6f
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-06-14 17:23:17 +0530

    Implement caption and transition to status.

--- carbonkivy/uix/notification/notification.py
+++ carbonkivy/uix/notification/notification.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
-__all__ = ("CNotificationInline", "CNotificationToast", "CNotificationCallout")
+__all__ = (
+    "CBaseNotification",
+    "CNotification",
+    "CNotificationCaption",
+    "CNotificationCloseButton",
+    "CNotificationInline",
+    "CNotificationToast",
+)
 
+from kivy.clock import Clock
 from kivy.properties import (
+    BooleanProperty,
     ColorProperty,
     NumericProperty,
     ObjectProperty,
@@ -13,27 +22,49 @@ from kivy.uix.modalview import ModalView
 
 from carbonkivy.behaviors import AdaptiveBehavior, DeclarativeBehavior
 from carbonkivy.uix.button import CButton
+from carbonkivy.uix.label import CLabel
+from carbonkivy.utils import get_latest_time
 
 
 class CBaseNotification(AdaptiveBehavior, DeclarativeBehavior, ModalView):
+
+    time_caption_enabled = BooleanProperty(False)
 
     _contrast_color = ColorProperty()
 
     _bgi_color = ColorProperty()
 
-    _bl_width = NumericProperty(1)
+    _bl_width = NumericProperty(3)
 
-    main_notification_layout = ObjectProperty()
+    status = OptionProperty("Success", options=["Error", "Info", "Success", "Warning"])
 
-    style = OptionProperty("Success", options=["Error", "Info", "Success", "Warning"])
-
-    caption = StringProperty()
+    caption = StringProperty(None, allownone=True)
 
     icon = StringProperty()
 
     title = StringProperty("Notification")
 
     subtitle = StringProperty()
+
+    cnotification_layout = ObjectProperty()
+
+    def on_time_caption_enabled(self, *args) -> None:
+        if self.time_caption_enabled and not self.caption:
+            self.caption = get_latest_time()
+
+    def on_caption(self, *args) -> None:
+
+        def add_caption(caption: CNotificationCaption, *args) -> None:
+            self.cnotification_layout.add_widget(caption)
+            self.ids["cnotification_caption"] = caption
+
+        if not self.ids.get("cnotification_caption"):
+            caption = CNotificationCaption(
+                text=self.caption,
+            )
+            Clock.schedule_once(lambda e: add_caption(caption))
+        else:
+            self.ids["cnotification_caption"].text = self.caption
 
 
 class CNotification(CBaseNotification):
@@ -50,4 +81,8 @@ class CNotificationToast(CBaseNotification):
 
 
 class CNotificationCloseButton(CButton):
+    pass
+
+
+class CNotificationCaption(CLabel):
     pass
