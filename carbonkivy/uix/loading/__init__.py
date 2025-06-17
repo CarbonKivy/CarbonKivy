9f0932eb5f7be020ad0621ed7e99a344cbc60107
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-06-17 17:24:45 +0530

    Update

--- carbonkivy/uix/loading/__init__.py
+++ carbonkivy/uix/loading/__init__.py
@@ -1,10 +1,9 @@
 import os
 
+from carbonkivy.config import UIX
 from kivy.lang import Builder
 
-from carbonkivy.config import UIX
-
-from .loading import CLoadingLayout, CLoadingIndicator
+from .loading import CLoadingIndicator, CLoadingLayout
 
 filename = os.path.join(UIX, "loading", "loading.kv")
 if not filename in Builder.files:
--- carbonkivy/uix/loading/loading.py
+++ carbonkivy/uix/loading/loading.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
-__all__ = ("CLoadingLayout", "CLoadingIndicator",)
+__all__ = (
+    "CLoadingLayout",
+    "CLoadingIndicator",
+)
 
 from kivy.clock import Clock
-from kivy.properties import ColorProperty, NumericProperty, OptionProperty
+from kivy.properties import (
+    BooleanProperty,
+    ColorProperty,
+    NumericProperty,
+    OptionProperty,
+)
 from kivy.uix.widget import Widget
 
 from carbonkivy.behaviors import HierarchicalLayerBehavior
@@ -16,6 +24,8 @@ class CLoadingLayout(CAnchorLayout, HierarchicalLayerBehavior):
 
 class CLoadingIndicator(Widget, HierarchicalLayerBehavior):
 
+    active = BooleanProperty(True)
+
     bg_color = ColorProperty([1, 1, 1, 0])
 
     stroke_color = ColorProperty()
@@ -26,9 +36,15 @@ class CLoadingIndicator(Widget, HierarchicalLayerBehavior):
 
     role = OptionProperty("Small", options=["Large", "Small"])
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super(CLoadingIndicator, self).__init__(**kwargs)
-        Clock.schedule_interval(self.update, 1/60)
+        self.on_active()
 
-    def update(self, dt):
+    def rotate(self, *args) -> None:
         self.angle = (self.angle + 6) % 360
+
+    def on_active(self, *args) -> None:
+        if self.active:
+            Clock.schedule_interval(self.rotate, 1 / 60)
+        elif not self.active:
+            Clock.unschedule(self.update)
