a0f6bfe087110a924f5ff3eb5d02e9dc3a10e818
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-07-01 17:47:14 +0530

    Implement tooltip

--- carbonkivy/behaviors/__init__.py
+++ carbonkivy/behaviors/__init__.py
@@ -8,3 +8,4 @@ from .declarative_behavior import DeclarativeBehavior
 from .hierarchical_layer_behavior import HierarchicalLayerBehavior
 from .hover_behavior import HoverBehavior
 from .state_focus_behavior import StateFocusBehavior
+from .tooltip_behavior import TooltipBehavior
---
+++ carbonkivy/uix/tooltip/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from kivy.lang import Builder
+
+from carbonkivy.config import UIX
+
+from .tooltip import (
+    CTooltip
+)
+
+filename = os.path.join(UIX, "tooltip", "tooltip.kv")
+if not filename in Builder.files:
+    Builder.load_file(filename)
