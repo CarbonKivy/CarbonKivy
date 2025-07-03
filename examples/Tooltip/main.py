ebd94b68c43d7c38a8a8fd8b92dcf23febcb0d66
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-07-03 16:27:56 +0530

    Update example

--- examples/Tooltip/main.py
+++ examples/Tooltip/main.py
@@ -13,11 +13,14 @@ appkv = """
 CScreen:
 
     TooltipButton:
+        text: "Drag Me"
         icon: "add--large"
-        pos_hint: {"center_x": 0.8, "center_y": 0.8}
+        pos: [30, 30]
 """
 
+from kivy.input.providers.mouse import MouseMotionEvent
 from kivy.lang import Builder
+from kivy.metrics import dp
 
 from carbonkivy.app import CarbonApp
 from carbonkivy.behaviors import TooltipBehavior
@@ -27,9 +30,14 @@ from carbonkivy.uix.tooltip import CTooltip
 
 
 class TooltipButton(CButtonPrimary, TooltipBehavior):
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.tooltip = CTooltip(text="This is a tooltip.")
+        self.tooltip = CTooltip(text="This is a tooltip.", width=dp(140))
+
+    def on_touch_move(self, touch: MouseMotionEvent, *args) -> bool | None:
+        self.center_x, self.center_y = touch.pos
+        return super().on_touch_move(touch)
 
 
 class myapp(CarbonApp):
