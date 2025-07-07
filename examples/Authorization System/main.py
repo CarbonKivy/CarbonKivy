c620237073b968354aaacb285e25d6d3addeca7a
kartavya <87070473+Novfensec@users.noreply.github.com>
2025-07-07 16:21:31 +0530

    Add example

---
+++ examples/Authorization System/main.py
@@ -0,0 +1,41 @@
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+def set_softinput(*args) -> None:
+    Window.keyboard_anim_args = {"d": 0.2, "t": "in_out_expo"}
+    Window.softinput_mode = "below_target"
+
+
+Window.on_restore(Clock.schedule_once(set_softinput, 0.1))
+
+
+from carbonkivy.app import CarbonApp
+from carbonkivy.uix.screen import CScreen
+from carbonkivy.uix.screenmanager import CScreenManager
+
+
+class UI(CScreenManager):
+    pass
+
+
+class MainScreen(CScreen):
+    pass
+
+
+class myapp(CarbonApp):
+    def __init__(self, *args, **kwargs):
+        super(myapp, self).__init__(*args, **kwargs)
+        self.load_all_kv_files(self.directory)
+
+    # def on_start(self, *args) -> None:
+    #     self.stop()
+
+    def build(self) -> CScreenManager:
+        self.manager_screens = CScreenManager()
+        self.manager_screens.add_widget(MainScreen(name="main"))
+        return self.manager_screens
+
+
+if __name__ == "__main__":
+    app = myapp()
+    app.run()
