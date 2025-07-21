14167d8ed74eb00fe148677b09b6c92a7cca7ee0
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-07-21 16:20:17 +0530

    Update main.py

--- examples/Product Page/main.py
+++ examples/Product Page/main.py
@@ -1,6 +1,8 @@
-from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.utils import get_color_from_hex
 
 
 def set_softinput(*args) -> None:
@@ -10,10 +12,23 @@ def set_softinput(*args) -> None:
 
 Window.on_restore(Clock.schedule_once(set_softinput, 0.1))
 
+
 from carbonkivy.app import CarbonApp
 from carbonkivy.uix.screen import CScreen
 from carbonkivy.uix.screenmanager import CScreenManager
 from carbonkivy.utils import update_system_ui
+from carbonkivy.uix.icon import CIconCircular
+from carbonkivy.uix.button import CButtonGhost
+from carbonkivy.behaviors import StateFocusBehavior
+
+
+class SelectableIcon(CIconCircular, StateFocusBehavior, ButtonBehavior):
+    def on_focus(self, *args) -> None:
+        super().on_focus(*args)
+        if self.focus:
+            for child in self.parent.children:
+                if child != self:
+                    child.focus = not self.focus
 
 
 class UI(CScreenManager):
@@ -30,11 +45,12 @@ class myapp(CarbonApp):
         self.load_all_kv_files(self.directory)
 
     def build(self) -> CScreenManager:
-        update_system_ui(self.layer_01, self.background, "Dark")
+        update_system_ui(get_color_from_hex("#B3E140"), self.background, "Dark")
         self.manager_screens = CScreenManager()
         self.manager_screens.add_widget(ProductScreen(name="product"))
         return self.manager_screens
 
-
+    
 if __name__ == "__main__":
-    myapp().run()
+    app = myapp()
+    app.run()
