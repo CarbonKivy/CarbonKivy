8843a0eaf6a4de6aa3c4fc188bd373a93984ffe4
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-08-05 14:10:39 +0530

    ADD CModal

---
+++ carbonkivy/uix/modal/modal.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from kivy.clock import Clock, mainthread
+from kivy.properties import (
+    BooleanProperty,
+    ColorProperty,
+    NumericProperty,
+    ObjectProperty,
+    OptionProperty,
+    StringProperty,
+)
+from kivy.uix.modalview import ModalView
+
+from carbonkivy.behaviors import (
+    AdaptiveBehavior,
+    DeclarativeBehavior,
+    ElevationBehavior,
+)
+from carbonkivy.uix.button import CButton
+from carbonkivy.uix.relativelayout import CRelativeLayout
+from carbonkivy.uix.stacklayout import CStackLayout
+from carbonkivy.uix.label import CLabel
+from carbonkivy.uix.shell import UIShellButton
+
+
+class CModal(
+    AdaptiveBehavior, DeclarativeBehavior, ModalView
+):
+    """
+    Base class for modal dialogs in CarbonKivy.
+    """
+
+
+class CModalLayout(CStackLayout):
+    pass
+
+
+class CModalHeader(CStackLayout):
+    pass
+
+
+class CModalHeaderLabel(CLabel):
+    pass
+
+
+class CModalHeaderTitle(CLabel):
+    pass
+
+
+class CModalBody(CStackLayout):
+    pass
+
+
+class CModalBodyContent(CLabel):
+    pass
+
+
+class CModalFooter(CStackLayout):
+    pass
+
+
+class CModalCloseButton(UIShellButton):
+    pass
