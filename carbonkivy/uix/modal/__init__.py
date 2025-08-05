9daa6c9e02838f1b3b4966e4ee19bcad3dd00686
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-08-05 14:10:39 +0530

    ADD CModal

---
+++ carbonkivy/uix/modal/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+from kivy.lang import Builder
+
+from carbonkivy.config import UIX
+
+from .modal import CModal, CModalBody, CModalBodyContent, CModalCloseButton, CModalFooter, CModalHeader, CModalHeaderLabel, CModalHeaderTitle, CModalLayout
+
+filename = os.path.join(UIX, "modal", "modal.kv")
+if not filename in Builder.files:
+    Builder.load_file(filename)
