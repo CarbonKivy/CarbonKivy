2e3e33be2a6aa831d01b2f2d9a27f6214e1993d4
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2025-12-07 14:40:43 +0530

    Updates

--- carbonkivy/uix/tab/tab.py
+++ carbonkivy/uix/tab/tab.py
@@ -1,12 +1,21 @@
 from __future__ import annotations
 
-__all__ = ("CTabManager", "CTab", "CTabLayout")
+__all__ = (
+    "CTab",
+    "CTabLayout",
+    "CTabManager",
+    "CTabHeader",
+    "CTabHeaderIcon",
+    "CTabHeaderItem",
+    "CTabHeaderItemLayout",
+    "CTabHeaderPrimaryText",
+    "CTabHeaderSecondaryText",
+)
 
 from typing import Any
 
 from kivy.clock import Clock
 from kivy.input.providers.mouse import MouseMotionEvent
-from kivy.metrics import sp
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.behaviors import ButtonBehavior
 
