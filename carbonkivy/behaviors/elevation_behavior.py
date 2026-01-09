ea6a53f71044a0068ee51d0bf4574805e03df7bc
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-01-09 11:51:34 +0530

    Fix elevation behavior

--- carbonkivy/behaviors/elevation_behavior.py
+++ carbonkivy/behaviors/elevation_behavior.py
@@ -5,19 +5,20 @@ __all__ = ("ElevationBehavior",)
 from kivy.event import EventDispatcher
 from kivy.lang import Builder
 from kivy.metrics import dp
-from kivy.properties import NumericProperty, VariableListProperty
+from kivy.properties import NumericProperty, VariableListProperty, ColorProperty
 
 Builder.load_string(
     """
 <ElevationBehavior>:
     canvas.before:
         Color:
-            rgba: app.notification_action_tertiary_inverse_hover
+            rgba: self.shadow_color
         BoxShadow:
             size: self.size
             pos: (self.pos[0] + self.shadow_offset[0], self.pos[1] + self.shadow_offset[1]) if not isinstance(self, RelativeLayout) else (self.shadow_offset[0], self.shadow_offset[1])
             offset: self.shadow_offset
             blur_radius: self.shadow_blur_radius
+    shadow_color: app.background_inverse_hover
 """
 )
 
@@ -27,6 +28,8 @@ class ElevationBehavior(EventDispatcher):
     shadow_offset = VariableListProperty([dp(1), -dp(1.5)], length=2)
 
     shadow_blur_radius = NumericProperty(0)
+
+    shadow_color = ColorProperty()
 
     def __init__(self, **kwargs) -> None:
         super(ElevationBehavior, self).__init__(**kwargs)
