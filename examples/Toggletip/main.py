d17e793bd6ef8a3372d324dde86f632b7046a245
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-02-10 18:02:02 +0530

    Update examples

--- examples/Toggletip/main.py
+++ examples/Toggletip/main.py
@@ -21,27 +21,43 @@ CScreen:
     CCheckbox:
         pos_hint: {"center_x": 0.5, "center_y": 0.5}
 
+<MToggletip>:
+    CLabel:
+        text: root.text
+        color: app.text_inverse
+    
+    CButtonPrimary:
+        text: "View more"
+        role: "Large Productive"
 """
 
 from kivy.input.providers.mouse import MouseMotionEvent
 from kivy.lang import Builder
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 
 from carbonkivy.app import CarbonApp
 from carbonkivy.behaviors import ElevationBehavior, TooltipBehavior
 from carbonkivy.uix.button import CButtonPrimary
 from carbonkivy.uix.screen import CScreen
+from carbonkivy.uix.toggletip import CToggletip
 from carbonkivy.uix.tooltip import CTooltip
+
+
+class MToggletip(CToggletip):
+
+    text = StringProperty()
 
 
 class TooltipButton(CButtonPrimary, ElevationBehavior, TooltipBehavior):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.tooltip = CTooltip(
-            text="This is a large Tooltip text.",
+        self.tooltip = MToggletip(
+            text="This is a large Toggletip text.",
             width=dp(288),
             margin=dp(2),
+            pointer="Upward",
         )
 
     def on_touch_move(self, touch: MouseMotionEvent, *args) -> bool | None:
