8bcca06c468249f1bbca7fd4c17e3765b1bb31f7
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-03-15 12:28:52 +0530

    Updates

--- carbonkivy/utils.py
+++ carbonkivy/utils.py
@@ -14,6 +14,102 @@ from carbonkivy.theme.size_tokens import (
     spacing_tokens,
 )
 
+if platform == "android":
+    from android.runnable import Runnable  # type: ignore
+    from jnius import PythonJavaClass, autoclass, java_method  # type: ignore
+
+    Color = autoclass("android.graphics.Color")
+    Build_VERSION = autoclass("android.os.Build$VERSION")
+    WindowInsetsType = autoclass("android.view.WindowInsets$Type")
+    PythonActivity = autoclass("org.kivy.android.PythonActivity")
+    View = autoclass("android.view.View")
+
+    activity = PythonActivity.mActivity
+    window = activity.getWindow()
+    decor_view = window.getDecorView()
+    content_view = window.findViewById(autoclass("android.R$id").content)
+
+    try:
+        WindowCompat = autoclass("androidx.core.view.WindowCompat")
+        inset_controller = WindowCompat.getInsetsController(window, decor_view)
+    except Exception as e:
+        inset_controller = None
+
+    def parse_color(value):
+        if isinstance(value, str):
+            return Color.parseColor(value)
+        elif isinstance(value, (list, tuple)) and len(value) == 4:
+            r, g, b, a = value
+            return Color.argb(a, r, g, b)
+        else:
+            raise ValueError("Color must be hex string or RGBA tuple")
+
+    if Build_VERSION.SDK_INT >= 35:
+
+        class InsetsListener(PythonJavaClass):
+            __javainterfaces__ = ["android/view/View$OnApplyWindowInsetsListener"]
+            __javacontext__ = "app"
+
+            def __init__(self, status_color, navigation_color, pad_status, pad_nav):
+                super().__init__()
+                self.status_color = status_color
+                self.navigation_color = navigation_color
+                self.pad_status = pad_status
+                self.pad_nav = pad_nav
+
+            @java_method(
+                "(Landroid/view/View;Landroid/view/WindowInsets;)Landroid/view/WindowInsets;"
+            )
+            def onApplyWindowInsets(self, view, insets):
+                try:
+                    status_insets = insets.getInsets(WindowInsetsType.statusBars())
+                    nav_insets = insets.getInsets(WindowInsetsType.navigationBars())
+                    top_pad = status_insets.top if self.pad_status else 0
+                    bottom_pad = nav_insets.bottom if self.pad_nav else 0
+                    content_view.setPadding(0, top_pad, 0, bottom_pad)
+                    content_view.setBackgroundColor(self.status_color)
+                    window.setNavigationBarColor(self.navigation_color)
+                except Exception as e:
+                    print("Insets error:", e)
+                    import traceback
+
+                    traceback.print_exc()
+                return insets
+
+        _global_listener: InsetsListener
+
+    def apply_legacy_flags(decor_view, icon_style):
+        visibility_flags = decor_view.getSystemUiVisibility()
+        if icon_style == "Light":
+            visibility_flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            if Build_VERSION.SDK_INT >= 26:
+                visibility_flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        else:
+            visibility_flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            if Build_VERSION.SDK_INT >= 26:
+                visibility_flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        decor_view.setSystemUiVisibility(visibility_flags)
+
+    def apply_insets_controller(window, decor_view, icon_style, inset_controller):
+        if inset_controller and "WindowInsetsControllerCompat" in str(
+            type(inset_controller)
+        ):
+            # AndroidX compat
+            light = icon_style == "Dark"
+            inset_controller.setAppearanceLightStatusBars(light)
+            inset_controller.setAppearanceLightNavigationBars(light)
+        else:
+            controller = window.getInsetsController()
+            WindowInsetsController = autoclass("android.view.WindowInsetsController")
+            mask = (
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+            )
+            if icon_style == "Light":
+                controller.setSystemBarsAppearance(0, mask)
+            else:
+                controller.setSystemBarsAppearance(mask, mask)
+
 
 def get_font_name(typeface: str, weight_style: str) -> str:
     font_dir = os.path.join(
@@ -57,130 +153,26 @@ def update_system_ui(
         https://github.com/Novfensec
     """
     if platform == "android":
-        from android.runnable import Runnable  # type: ignore
-        from jnius import PythonJavaClass, autoclass, java_method  # type: ignore
-
-        Color = autoclass("android.graphics.Color")
-        Build_VERSION = autoclass("android.os.Build$VERSION")
-        WindowInsetsType = autoclass("android.view.WindowInsets$Type")
-        PythonActivity = autoclass("org.kivy.android.PythonActivity")
-        View = autoclass("android.view.View")
-
-        activity = PythonActivity.mActivity
-        window = activity.getWindow()
-        decor_view = window.getDecorView()
-        content_view = window.findViewById(autoclass("android.R$id").content)
-
-        try:
-            WindowCompat = autoclass("androidx.core.view.WindowCompat")
-            inset_controller = WindowCompat.getInsetsController(window, decor_view)
-        except Exception as e:
-            inset_controller = None
-
-        def parse_color(value):
-            if isinstance(value, str):
-                return Color.parseColor(value)
-            elif isinstance(value, (list, tuple)) and len(value) == 4:
-                r, g, b, a = value
-                return Color.argb(a, r, g, b)
-            else:
-                raise ValueError("Color must be hex string or RGBA tuple")
 
         def apply_system_bars():
             status_color_int = parse_color(status_bar_color)
             navigation_color_int = parse_color(navigation_bar_color)
 
-            if Build_VERSION.SDK_INT >= 30:
-                # API 30+ (Android 10+)
-                if inset_controller and "WindowInsetsControllerCompat" in str(
-                    type(inset_controller)
-                ):
-                    # Compat wrapper (AndroidX)
-                    if icon_style == "Light":
-                        inset_controller.setAppearanceLightStatusBars(False)
-                        inset_controller.setAppearanceLightNavigationBars(False)
-                    else:
-                        inset_controller.setAppearanceLightStatusBars(True)
-                        inset_controller.setAppearanceLightNavigationBars(True)
-                else:
-                    # Platform controller (API 30+)
-                    controller = window.getInsetsController()
-                    WindowInsetsController = autoclass(
-                        "android.view.WindowInsetsController"
-                    )
-                    if icon_style == "Light":
-                        controller.setSystemBarsAppearance(
-                            0,
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                            | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                        )
-                    else:
-                        controller.setSystemBarsAppearance(
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                            | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                            | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                        )
-
-            else:
-                # Legacy flags for API 23–29
-                visibility_flags = decor_view.getSystemUiVisibility()
-
-                if icon_style == "Light":
-                    visibility_flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                    if Build_VERSION.SDK_INT >= 26:
-                        visibility_flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                else:
-                    visibility_flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                    if Build_VERSION.SDK_INT >= 26:
-                        visibility_flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-
-                decor_view.setSystemUiVisibility(visibility_flags)
-
             if Build_VERSION.SDK_INT >= 35:
-
-                class InsetsListener(PythonJavaClass):
-                    __javainterfaces__ = [
-                        "android/view/View$OnApplyWindowInsetsListener"
-                    ]
-                    __javacontext__ = "app"
-
-                    def __init__(self, status_color, navigation_color):
-                        super().__init__()
-                        self.status_color = status_color
-                        self.navigation_color = navigation_color
-
-                    @java_method(
-                        "(Landroid/view/View;Landroid/view/WindowInsets;)Landroid/view/WindowInsets;"
-                    )
-                    def onApplyWindowInsets(self, view, insets):
-                        try:
-                            status_insets = insets.getInsets(
-                                WindowInsetsType.statusBars()
-                            )
-                            nav_insets = insets.getInsets(
-                                WindowInsetsType.navigationBars()
-                            )
-
-                            top_pad = status_insets.top if pad_status else 0
-                            bottom_pad = nav_insets.bottom if pad_nav else 0
-
-                            content_view.setPadding(0, top_pad, 0, bottom_pad)
-                            content_view.setBackgroundColor(self.status_color)
-
-                            window.setNavigationBarColor(self.navigation_color)
-                        except Exception as e:
-                            print("Insets error:", e)
-                            import traceback
-
-                            traceback.print_exc()
-                        return insets
-
-                listener = InsetsListener(status_color_int, navigation_color_int)
-                activity._system_ui_listener = listener
-                decor_view.setOnApplyWindowInsetsListener(listener)
+                global _global_listener
+                _global_listener = InsetsListener(
+                    status_color_int, navigation_color_int, pad_status, pad_nav
+                )
+                decor_view.setOnApplyWindowInsetsListener(_global_listener)
                 decor_view.requestApplyInsets()
+            if Build_VERSION.SDK_INT >= 30:
+                apply_insets_controller(
+                    window, decor_view, icon_style, inset_controller
+                )
+                window.setStatusBarColor(status_color_int)
+                window.setNavigationBarColor(navigation_color_int)
             else:
+                apply_legacy_flags(decor_view, icon_style)
                 window.setStatusBarColor(status_color_int)
                 window.setNavigationBarColor(navigation_color_int)
 
