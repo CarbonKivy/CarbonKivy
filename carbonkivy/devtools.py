cca05aa53cc153a8e4468fad348d2873b8392290
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-03-23 08:52:23 +0530

    Updates

--- carbonkivy/devtools.py
+++ carbonkivy/devtools.py
@@ -4,6 +4,7 @@ import os
 from kaki.app import App
 from kivy.clock import mainthread
 from kivy.factory import Factory
+from kivy.metrics import sp
 
 
 class LiveApp(App):
@@ -25,11 +26,16 @@ class LiveApp(App):
     @mainthread
     def set_error(self, exc, tb=None):
         from kivy.core.window import Window
-
+        clean_exc = exc[:1000].replace('\\n', '\n')
+        clean_tb = (tb[:1000] or '').replace('\\n', '\n')
+        output = '{}\n\n{}'.format(clean_exc[:1000], clean_tb[:1000] or '')
         lbl = Factory.CLabel(
+            markup=True,
             padding=16,
-            text="{}\n\n{}".format(exc, tb or ""),
+            typeface="IBM Plex Mono",
+            text=f"{output}",
         )
+        lbl.font_size=sp(12)
         lbl.texture_update()
         sv = Factory.ScrollView(
             size_hint=(1, 1), pos_hint={"x": 0, "y": 0}, do_scroll_x=False, scroll_y=0
