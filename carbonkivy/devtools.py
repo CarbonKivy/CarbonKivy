1391fe9af2ba6223fa2f972959fdb5daf47df0e9
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-03-26 13:58:22 +0530

    Update Live app debug logs

--- carbonkivy/devtools.py
+++ carbonkivy/devtools.py
@@ -26,16 +26,17 @@ class LiveApp(App):
     @mainthread
     def set_error(self, exc, tb=None):
         from kivy.core.window import Window
-        clean_exc = exc[:1000].replace('\\n', '\n')
-        clean_tb = (tb[:1000] or '').replace('\\n', '\n')
-        output = '{}\n\n{}'.format(clean_exc[:1000], clean_tb[:1000] or '')
+
+        clean_exc = exc[:1000].replace("\\n", "\n")
+        clean_tb = (tb[:1000] or "").replace("\\n", "\n")
+        output = "{}\n\n{}".format(clean_exc[:1000], clean_tb[:1000] or "")
         lbl = Factory.CLabel(
             markup=True,
             padding=16,
             typeface="IBM Plex Mono",
             text=f"{output}",
         )
-        lbl.font_size=sp(12)
+        lbl.font_size = sp(12)
         lbl.texture_update()
         sv = Factory.ScrollView(
             size_hint=(1, 1), pos_hint={"x": 0, "y": 0}, do_scroll_x=False, scroll_y=0
